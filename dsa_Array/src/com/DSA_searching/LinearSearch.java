fe2924a34fae36c1b1bbe2d8fe640a0c586d4d08
Mayurrajput97 <mayurrajput7827@gmail.com>
2024-03-16 14:19:58 +0530

    Linear Search

--- dsa_Array/src/com/DSA_searching/LinearSearch.java
+++ dsa_Array/src/com/DSA_searching/LinearSearch.java
@@ -1,26 +1,25 @@
 package com.DSA_searching;
 
 public class LinearSearch {
-	
-	public static int linearSerching(int [] arr, int target) {
-		
-		for(int i = 0; i <= arr.length; i++) {
-			
-			if(arr[i] == target) {
+
+	public static int linearSerching(int[] arr, int target) {
+
+		for (int i = 0; i <= arr.length; i++) {
+
+			if (arr[i] == target) {
 				return i;
-				
+
 			}
 		}
 		return -1;
-		
 	}
 
 	public static void main(String[] args) {
-		int [] arr = {2,4,7,9,9,44,79};
+		int[] arr = { 2, 4, 7, 9, 9, 44, 79 };
 		int target = 9;
-		int index  = linearSerching(arr, target);
+		int index = linearSerching(arr, target);
 		System.out.println("The Element 9 Found At index : " + index);
-		
+
 	}
 
 }
