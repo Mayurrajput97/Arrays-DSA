88ddcd99a758cd3b2920d82d30d6a21127a5d6de
Mayurrajput97 <mayurrajput7827@gmail.com>
2024-03-16 15:12:31 +0530

    Dublicates

---
+++ dsa_Array/src/com/DSA_searching/Finding_Dublicates.java
@@ -0,0 +1,24 @@
+package com.DSA_searching;
+
+public class Finding_Dublicates {
+	public static boolean findiDublicates(int []arr) {
+		
+		for(int i =0; i< arr.length; i++) {	
+			for(int j = i +1 ; j <arr.length; j++) {
+				
+				if(arr[i] == arr[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+		int [] arr = {2,4,6,7,4};
+		
+		System.out.println("The Array Has Dublicates? : "  + findiDublicates(arr));
+	}
+
+}
